986eac6aa5ea6fa65a7bd5454882d488a89058d1
Jordan Watson <jordan.watson@verint.com>
2018-03-09 14:33:32 +0000

    Test suite added fir form component checking change state

--- src/script/components/_form.test.js
+++ src/script/components/_form.test.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import { shallowToJson } from "enzyme-to-json";
 
 import FormSearch from './_form';
 
-describe("<FormSearch />", () => {
-    it("should render correctly", () => {
-        const output = shallow(<FormSearch title="mockTitle" url="mockUrl" />);
-        expect(shallowToJson(output)).toMatchSnapshot();
+describe('Form Search', () => {
+    let props;
+    let mountedForm;
+
+    // Form Element
+    const Form = shallow( <FormSearch /> );
+    
+    beforeEach( () => {
+        props = {
+            placeholder: undefined,
+            clearOnSubmit: undefined
+        };
+    });
+
+    // Form Element to exists
+    it('Form element to exists', () => {
+        expect( Form.find('form').length ).toEqual(1);
+    });
+
+    // Form input change
+    it('Should update state when form input is changed', () => {
+        const input = Form.find('input');
+        input.simulate('change', {target: {value: 'Form search added'}});
+        expect(Form.state().value).toEqual('Form search added');
     });
 });
